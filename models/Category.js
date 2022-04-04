4e412fabb375f94b6f6d310b0d320d3a1c85bcc1
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-04-04 14:03:32 -0400

    added the wrong folders

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { INTEGER } = require('sequelize/types');
 
 const sequelize = require('../config/connection.js');
 
@@ -7,6 +8,11 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: INTEGER,
+    value: not_null; //! double check this . likely wrong
+    primaryKey: , //! double check this . likely wrong
+    use: auto_increment, //! double check this . likely wrong
+    category_name, //! double check this . likely wrong
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
-
 // Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
+
 
 module.exports = {
   Product,
