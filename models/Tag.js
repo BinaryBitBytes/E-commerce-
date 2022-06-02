dabdd145ff6b44e0677d5eb9caf1022a7a71dffa
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-02 13:23:24 -0400

    added comment
    added comments

--- models/Tag.js
+++ models/Tag.js
@@ -7,20 +7,20 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-     type: DataTypes.INTEGER,
-     primaryKey: true,
-     autoIncrement: true
+     type: DataTypes.INTEGER,  // declared tag id as an integer
+     primaryKey: true, //tag id is primary key
+     autoIncrement: true // auto incrementation on tag id
    },
    tag_name: {
-     type: DataTypes.STRING
+     type: DataTypes.STRING // tdeclared tag name as a string
    },
  },
   {
-    sequelize,
+    sequelize, // using sequelize
     timestamps: false,
-    freezeTableName: true,
+    freezeTableName: true, // freezing table names
     underscored: true,
-    modelName: 'tag',
+    modelName: 'tag', // model name as tag
   }
 );
 
