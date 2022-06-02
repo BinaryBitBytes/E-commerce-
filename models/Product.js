ea32b86649117f5e87eec58b22975e495e11ca23
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-06-02 13:20:58 -0400

    commented changes
    added comments

--- models/Product.js
+++ models/Product.js
@@ -3,38 +3,36 @@ const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+      type: DataTypes.INTEGER, // product id is an interger
+      primaryKey: true, // this is the primary key, being the ID
       autoIncrement: true
     },
     product_name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING, //product name is a string
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL(10,2),
+      type: DataTypes.DECIMAL(10,2), // declares decimal
       allowNull: false,
       validate: {
         isDecimal: true
       }
     },
     stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+      type: DataTypes.INTEGER, // display stock in an interger
+      allowNull: false, // cannot be null
       defaultValue: 10,
       validate: {
         isNumeric: true
       }
     },
     category_id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, // this is the category ID as an interger
       references: {
         model: 'category',
         key: 'id',
